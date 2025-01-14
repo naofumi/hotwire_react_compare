c0e0cf27b0e46370a2e5eee237b5242392019c11
Naofumi Kagami <naofumi@mac.com>
2025-01-14 15:47:41 +0900

    minor UI fixes

--- app/javascript/react/components/PostComponent.tsx
+++ app/javascript/react/components/PostComponent.tsx
@@ -4,17 +4,17 @@ import type {Post} from "@/app/javascript/react/models/post"
 export default function PostComponent({post}: { post: Post }) {
   return <div id={`post-${post.id}`}>
     <p>
-      <strong>Title:</strong>
+      <strong>Title: </strong>
       {post.title}
     </p>
 
     <p>
-      <strong>Body:</strong>
+      <strong>Body: </strong>
       {post.body}
     </p>
 
     <p>
-      <strong>Author name:</strong>
+      <strong>Author Name: </strong>
       {post.author.name}
     </p>
 
